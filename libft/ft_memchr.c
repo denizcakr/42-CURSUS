fb9e6404d9fa89e7a5cd7d17d5f446eccf68f06d
Ezgi Deniz Çakır <114353663+denizcakr@users.noreply.github.com>
2023-01-08 13:29:57 +0000

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -25,3 +25,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (0);
 }
+
+// s parametresi ile gösterilen bellek bölgesinin n parametre değeri kadar ilk byte'ına, 
+// c parametresi ile gösterilen değerin (unsigned char olarak değerlendirilir) ilk geçtiği yeri arar ve
+// bu bellek adresini gösteren bir işaretçi geri döndürür.
+// Eğer const void'e atanan parametre 'null' bir işaretçi ise anlamsız sonuç elde edilir.
